89eb66f9a1ae557afb62de2fdfcc75c2db915927
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-01 14:53:14 +0500

    output manage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print.js';
 
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-function component() {
-  const element = document.createElement('div');
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-  return element;
-}
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-document.body.appendChild(component());
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
